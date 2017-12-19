407cf81107cacfe22257a20780a22182814cad9e
Teguh Santoso <teguh.santoso@gmx.de>
2017-12-19 11:10:43 +0100

    Minor bugfixing and clean up code.

--- app/src/main/java/de/tsa/homecosts/entities/CategoryType.java
+++ app/src/main/java/de/tsa/homecosts/entities/CategoryType.java
@@ -1,24 +1,18 @@
 package de.tsa.homecosts.entities;
 
-/**
- * Enumeration class for device types.
- *
- * @author AppPlusMobile Systemhaus GmbH 2017, all rights reserved, any use is prohibited.
- * @since 1.1
- *
- */
 public enum CategoryType {
 
     INCOME {
         @Override
         public String toString() {
-            return "EINKOMMEN";
+            return "Income";
         }
     },
     OUTCOME {
         @Override
         public String toString() {
-            return "AUSGABE";
+            return "Expenditure";
         }
     }
+
 }
